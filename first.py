314ded5da9fe19c356de2a6b1155a6c9163e0b92
steve-soares <steve.coubertin@gmail.com>
2019-11-07 11:39:50 +0100

    "dossier python"

---
+++ first.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+print("hello world")
+
+a=1
+if a == 0:
+    print("toto")
+else:
+    print("a ne vaut pas 0")
+
+print("fin")
---
+++ jour2/module1/result_calcul.py
@@ -0,0 +1,11 @@
+import calcul
+
+print(calcul.addition(3, 7))
+print(calcul.soustraction(4, 2))
+
+
+from calcul import addition
+from calcul import soustraction
+
+print(addition(3, 7))
+print(soustraction(4, 2))
